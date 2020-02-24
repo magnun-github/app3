1ab82a97aba8d3254354c8e6eb8c6ecec2adeba9
magnun <jesus.of.suburbia6969@gmail.com>
2020-02-24 22:30:32 +0000

    made-homepage

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # ページごとの完全なタイトルを返します。
   def full_title(page_title = '')
-    base_title = "Lantern Lantern"
+    base_title = "Growth"
     if page_title.empty?
       base_title
     else
