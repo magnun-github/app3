4a26ca24c9924c71dfef329fc3107a0fb26e79ef
magnun <jesus.of.suburbia6969@gmail.com>
2020-02-25 02:22:16 +0000

    model validation test

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,14 @@ module App3
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    
+    config.generators.test_framework      = :rspec
+    config.generators.system_tests        = false
+    config.generators.stylesheets         = false
+    config.generators.javascripts         = false
+    config.generators.helper              = false
+    config.generators.helper_specs        = false
+    config.generators.view_specs          = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
