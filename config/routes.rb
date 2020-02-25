9d1dffb794b628e1913749f7e9b616dc8b7534ec
magnun <jesus.of.suburbia6969@gmail.com>
2020-02-25 09:05:44 +0000

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/about'
   get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
   resources :users
 end
