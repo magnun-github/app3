e805b89567617d6b429a3e0498e4b520c3a90271
magnun <jesus.of.suburbia6969@gmail.com>
2020-02-25 00:46:00 +0000

    link test

--- spec/systems/site_layout_spec.rb
+++ spec/systems/site_layout_spec.rb
@@ -1,18 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe "SiteLayouts", type: :system do
+  
+  describe "home layout" do    
+    it "contains root link" do
+      visit root_path
+      expect(page).to have_link nil, href: root_path, count: 1
+    end
+
+    it "contains signup link" do
+      visit root_path
+      expect(page).to have_link 'はじめる', href: signup_path
+    end
+
+    it "contains login link" do
+      visit root_path
+      expect(page).to have_link 'Login', href: login_path
+    end
 
   describe "home layout" do
-    it "returns title with 'Lantern Lantern'" do
+    it "returns title with 'Growth'" do
       visit root_path
-      expect(page).to have_title 'Lantern Lantern'
+      expect(page).to have_title 'Growth'
     end
   end
 
   describe "about layout" do
-    it "returns title with 'About | Lantern Lantern'" do
+    it "returns title with 'About | Growth'" do
       visit about_path
-      expect(page).to have_title 'About | Lantern Lantern'
+      expect(page).to have_title 'About | Growth'
     end
   end
+ end
 end
