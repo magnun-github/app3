8d76c14cdeeb740d55a3dc4691c32bfba64c0d9d
magnun <jesus.of.suburbia6969@gmail.com>
2020-02-26 02:39:05 +0000

    Implement basic login

--- spec/requests/users_signups_spec.rb
+++ spec/requests/users_signups_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe "UsersSignup", type: :request do
+  include SessionsHelper
+  
   describe "GET /signup" do
     it "is invalid signup information" do
       get signup_path
@@ -14,6 +16,7 @@ RSpec.describe "UsersSignup", type: :request do
           }
         }
       }.not_to change(User, :count)
+      expect(is_logged_in?).to be_falsey
     end
     
     it "is valid signup information" do
@@ -28,6 +31,7 @@ RSpec.describe "UsersSignup", type: :request do
           }
         }
       }.to change(User, :count).by(1)
+      expect(is_logged_in?).to be_truthy
     end
   end
 end
