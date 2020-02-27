96cdc67c3559be22cc2462e73a93a0a45adc0ae8
magnun <jesus.of.suburbia6969@gmail.com>
2020-02-27 01:59:38 +0000

    Add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,13 +10,12 @@ class UsersController < ApplicationController
     @user = User.new
   end
   
-  
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = "Growthへようこそ！"
-      redirect_to @user
+      UserMailer.account_activation(@user).deliver_now
+      flash[:info] = "認証用メールを送信しました。登録時のメールアドレスから認証を済ませてください"
+      redirect_to root_url
     else
       render 'new'
     end
