6a659c64ba2e1b2f852d72fed2bb60b78b0d1843
magnun <jesus.of.suburbia6969@gmail.com>
2020-02-28 07:19:34 +0000

    pagenated

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,21 @@
 FactoryBot.define do
   factory :user do
     name { "Michael Example" }
-    email { "michael@example.com" }
+    sequence(:email) { |n| "michael_#{n}@example.com" }
     password { "password" }
     password_confirmation { "password" }
   end
 
   factory :other_user, class: User do
     name { "Sterling Archer" }
-    email { "duchess@example.gov" }
+    sequence(:email) { |n| "duchess_#{n}@example.gov" }
+    password { "foobar" }
+    password_confirmation { "foobar" }
+  end
+
+  factory :no_activation_user, class: User do
+    name { "No Activation" }
+    sequence(:email) { |n| "no_#{n}@activation.co.jp" }
     password { "foobar" }
     password_confirmation { "foobar" }
   end
